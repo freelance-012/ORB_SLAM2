d35178409b3e9f37a7e17b27147725a58579866f
shanggaoxing <shanggaoxing@sf-express.com>
2025-11-20 18:24:36 +0800

    update .gitignore; add options.cmake for build

---
+++ options.cmake
@@ -0,0 +1,7 @@
+set(OpenCV_DIR "~/03_install_dir/01_opencv/opencv-3.4.15/share/OpenCV" CACHE PATH "")
+set(opengv_DIR "~/03_install_dir/05_opengv/lib/cmake/opengv-1.0" CACHE PATH "")
+set(glog_DIR "~/03_install_dir/02_glog/lib/cmake/glog" CACHE PATH "")
+set(ceres_DIR "~/03_install_dir/03_ceres-solver/lib/cmake/Ceres" CACHE PATH "")
+set(Sophus_DIR "~/03_install_dir/04_Sophus/share/sophus/cmake" CACHE PATH "")
+set(Pangolin_DIR "~/03_install_dir/08_pangolin/Pangolin-0.6/lib/cmake/Pangolin" CACHE PATH "")
+
